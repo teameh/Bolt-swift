e7575a72cad49e366eb3673bf7b1a989b9fedc4c
Tieme van Veen <tiemevanveen@hotmail.com>
2019-11-30 22:49:27 +0100

    Revive 1.0.2 by using swift-tools-version 4.0

--- Package.swift
+++ Package.swift
@@ -1,10 +1,20 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "Bolt",
-	dependencies: [
-	    .Package(url: "https://github.com/Neo4j-Swift/PackStream-swift.git",
-	                 majorVersion: 1),
-		.Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0),
-	    ]
+    products: [
+        .library(name: "Bolt", targets: ["Bolt"]),
+    ],
+    dependencies: [
+      .package(url: "https://github.com/Neo4j-Swift/PackStream-swift.git", from: "1.0.0"),
+      .package(url: "https://github.com/IBM-Swift/BlueSSLService.git", from: "0.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "Bolt",
+            dependencies: ["PackStream", "SSLService"],
+            path: "Sources"
+        )
+    ]
 )
